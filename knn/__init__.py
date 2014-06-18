5e93e9d0388435f341e00374dde774bd662f1fbb
ccravens <ccravens@CCRAV-CHS-E6420.LCE.com>
2014-06-18 10:02:11 -0400

    Updated kNN to support a plot using matplotlib

--- knn/__init__.py
+++ knn/__init__.py
@@ -1,5 +1,16 @@
 import kNN
+import matplotlib
+import matplotlib.pyplot as plt
 
-group,labels = kNN.createDataSet()
+datingDataMat,datingLabels = kNN.file2matrix("test.txt")
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+plt.show()
 
-print(kNN.classify0([1.0,1.0], group, labels, 3))
+# group,labels = kNN.createDataSet()
+# 
+# print(group)
+# print(labels)
+# 
+# print(kNN.classify0([0.0,1.1], group, labels, 3))
